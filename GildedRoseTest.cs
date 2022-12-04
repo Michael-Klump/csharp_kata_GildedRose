a483432df9fd7ef371fff55c511c89feb2c9f505
Michael-Klump <119431443+Michael-Klump@users.noreply.github.com>
2022-12-04 18:05:55 +0100

    Added NUnit Tests
    NUnit Test #1
    Create a file with the expected values for the 4 items (3 specific and one general) for the course of 30 days
    NUnit Test #2
    Compare the results for the expected values for the 4 items over the course of 30 days

--- GildedRoseTest.cs
+++ GildedRoseTest.cs
@@ -1,18 +1,64 @@
 using NUnit.Framework;
+using System;
 using System.Collections.Generic;
+using System.IO;
 
 namespace csharp
 {
     [TestFixture]
     public class GildedRoseTest
     {
+
         [Test]
-        public void foo()
+        public void ProduceCurrentTestResults()
         {
-            IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 0 } };
+            IList<Item> Items = new List<Item> {
+                new Item { Name = "Aged Brie", SellIn = 2, Quality = 0 },
+                new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 15, Quality = 20 },
+                new Item { Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80 },
+                new Item { Name = "Other Items", SellIn = 10, Quality = 20 }
+            };
+
             GildedRose app = new GildedRose(Items);
-            app.UpdateQuality();
-            Assert.AreEqual("fixme", Items[0].Name);
+
+            for (int DayNumber = 1; DayNumber <= 30; DayNumber++)
+            {
+                app.UpdateQuality();
+                foreach (Item CurrentItem in Items)
+                {
+                    Console.WriteLine(CurrentItem.Name + "," + CurrentItem.SellIn + "," + CurrentItem.Quality);
+                }
+            }
+        }
+
+        [Test]
+        public void UpdateQualityTest()
+        {
+
+
+
+            IList<Item> Items = new List<Item> {
+                new Item { Name = "Aged Brie", SellIn = 2, Quality = 0 },
+                new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 15, Quality = 20 },
+                new Item { Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80 },
+                new Item { Name = "Other Items", SellIn = 10, Quality = 20 }
+            };
+
+            GildedRose app = new GildedRose(Items);
+
+            string[] CurrentTestResultsLines = File.ReadAllLines("CurrentTestResults.txt");
+            int CurrentTestResultLineCounter = 0;
+
+            for (int DayNumber = 1; DayNumber <= 30; DayNumber++)
+            {
+                app.UpdateQuality();
+                foreach (Item CurrentItem in Items)
+                {
+                    Assert.AreEqual(CurrentTestResultsLines[CurrentTestResultLineCounter],
+                        CurrentItem.Name + "," + CurrentItem.SellIn + "," + CurrentItem.Quality);
+                    CurrentTestResultLineCounter++;
+                }
+            }
         }
     }
 }
